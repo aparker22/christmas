833743ca91a03829b6263f18850e23548ba6d32a
Ashley <aparker@riskalyze.com>
2019-12-21 11:20:27 -0500

    trying again

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login  from "./login/login";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./content/home/home"
 import Footer from "./footer/footer";
 
 function App() {
   return (
-    <Router className="App">
     <div className="App">
-      <div className="body">
-      <Route path="/" exact component={Home} />
-        <Route path="/login" exact component={Login} />
-      </div>
+      <Home />
       <Footer/>
     </div>
-  </Router>
   );
 }
 
--- src/content/home/home.js
+++ src/content/home/home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './home.css'
 import Header from '../../header/header';
+import family from '../photos/family.jpg'
 
 const Home = () => {
     return (
         <div className="home-container">
             <Header/>
-            <div>Merry Christmas!</div>
+            <div className="header-content">
+                <h1 className="header-text">Merry Christmas Mimi and Granddaddy!</h1>
+                <h3 className="header-text">We Love you!</h3>
+                <img src={family} alt="family" className="header-img" />
+            </div>
         </div>)
 };
 
