5783f992b198690c4df400a3d8b68e5cca9899f2
Ashley <aparker@riskalyze.com>
2019-12-21 10:00:39 -0500

    Updates

---
+++ precache-manifest.c56dd7486cefc6f73200c92f7671d428.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53782dc99f7bed4f158ffe81178c8e34",
+    "url": "/christmas/index.html"
+  },
+  {
+    "revision": "a6fb5a803d6296498cef",
+    "url": "/christmas/static/css/main.3f10fd45.chunk.css"
+  },
+  {
+    "revision": "9e28e2f1438ffe887c57",
+    "url": "/christmas/static/js/2.081216e6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/christmas/static/js/2.081216e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6fb5a803d6296498cef",
+    "url": "/christmas/static/js/main.6a29491a.chunk.js"
+  },
+  {
+    "revision": "a16e17a2d58bc18b6d08",
+    "url": "/christmas/static/js/runtime-main.e516d7e5.js"
+  },
+  {
+    "revision": "a45e442764f06943b7bce83a9b292081",
+    "url": "/christmas/static/media/background.a45e4427.jpg"
+  }
+]);
